c42d3ef2016ebb3689307684ec880ddea2128b65
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-05-25 21:11:37 +0900

    2023-05-25 오늘의 코딩테스트 문제풀이 정리

--- lv1/coding-test.md
+++ lv1/coding-test.md
@@ -124,6 +124,22 @@ def solution(n, m, section):
     return answer
 ```
 
+## 카드 뭉치
+
+```py
+def solution(cards1, cards2, goal):
+
+    for g in goal:
+        if cards1 and cards1[0] == g:
+            cards1.pop(0)
+        elif cards2 and cards2[0] == g:
+            cards2.pop(0)
+        else:
+            return 'No'
+
+    return 'Yes'
+```
+
 ## 문제 이름
 
 ```py
--- note.py
+++ note.py
@@ -1,12 +1,12 @@
-def solution(q, r, code):
-    answer = ''
-    for i in range(len(code)):
-        if i % q == r:
-            answer += code[i]
-    return answer
 
-q = 3
-r = 1
-code = "qjnwezgrpirldywt"
-print(solution(q, r, code))
-# result = jerry
+def solution(cards1, cards2, goal):
+        
+    for g in goal:
+        if cards1 and cards1[0] == g:
+            cards1.pop(0)
+        elif cards2 and cards2[0] == g:
+            cards2.pop(0)
+        else:
+            return 'No'
+
+    return 'Yes'
