c6a576ce108a96944e675cd598c689322d739fd0
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-06-16 17:52:49 +0900

    2023-06-16 카카오 코딩테스트

--- note.py
+++ note.py
@@ -1,3 +1,5 @@
+import copy
+
 def solution(today, terms, privacies):
 
     split_terms = list(map(lambda x:x.split(" "),terms))
@@ -14,23 +16,29 @@ def solution(today, terms, privacies):
         
         to_day = list(map(int,today.split(".")))
         start_day = list(map(int,privacy["day"].split(".")))
-        end_day = start_day
+        end_day = copy.deepcopy(start_day)
 
-        # 0월 0일은 없다.
-        
         if start_day[1] + term > 12:
             end_day[1] = (start_day[1] + term) - (12 * ((start_day[1] + term) // 12))
             end_day[0] = end_day[0] + ((start_day[1] + term) // 12)
         else:
             end_day[1] = start_day[1] + term
         
-        # end_day와 today의 차이를 구해보자        
-
+        to_day_length = (to_day[0] * 12 * 28) + (to_day[1] * 28) + to_day[2]
+        end_day_length = (end_day[0] * 12 * 28) + (end_day[1] * 28) + end_day[2]
+        
+        if end_day_length - to_day_length > 0 :
+            result.append(i)
+        
     return result
 
-today = "2022.05.19"
-terms = ["A 6","B 12","C 3"]
-privacies = ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]
+# today = "2022.05.19"
+# terms = ["A 6","B 12","C 3"]
+# privacies = ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]
+
+today = "2020.01.01"
+terms = ["Z 3", "D 5"]
+privacies = ["2019.01.01 D", "2019.11.15 Z", "2019.08.02 D", "2019.07.01 D", "2018.12.28 Z"]
 
 
 # 2022.05.19
