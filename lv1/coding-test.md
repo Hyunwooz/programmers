60576b35bfee5ec195257d0d14081e6966f8d624
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2023-10-24 17:47:09 +0900

    Lv1 소수 만들기

--- lv1/coding-test.md
+++ lv1/coding-test.md
@@ -705,3 +705,33 @@ def solution(s):
             return True
     return False
 ```
+
+### 소수 만들기
+
+```py
+from itertools import *
+
+def prime(n):
+    result = []
+
+    for i in range(1, int(n**(1/2))+1):
+        if n % i == 0:
+            result.append(i)
+            if ((i**2) != n) :
+                result.append(n//i)
+                
+    if len(result) == 2:
+        return True
+    else:
+        return False
+
+def solution(nums):
+    numlist = list(combinations(nums, 3))
+    answer = 0
+    
+    for num in numlist:
+        if prime(sum(num)):
+            answer += 1
+
+    return answer
+```
--- note.py
+++ note.py
@@ -1,9 +1,25 @@
-def solution(n):
-    
-    num_list = set(i for i in range(2,n+1)) 
-    for i in range(2, int(n ** 1/2) + 1):
-        if i in num_list:
-            num_list -= set(i for i in range(i*2, n+1,i))
-    return len(num_list)
+from itertools import *
 
-print(solution(10000))
+def prime(n):
+    result = []
+
+    for i in range(1, int(n**(1/2))+1):
+        if n % i == 0:
+            result.append(i)
+            if ((i**2) != n) :
+                result.append(n//i)
+                
+    if len(result) == 2:
+        return True
+    else:
+        return False
+
+def solution(nums):
+    numlist = list(combinations(nums, 3))
+    answer = 0
+    
+    for num in numlist:
+        if prime(sum(num)):
+            answer += 1
+
+    return answer
