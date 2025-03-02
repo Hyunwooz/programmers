cd5ebdcc96b8ad17ca7f3b102a0813c65116a618
Hyunwooz <107661525+Hyunwooz@users.noreply.github.com>
2025-03-02 16:25:43 +0900

    Lv1 [PCCP 기출문제] 1번 / 붕대 감기

--- note.py
+++ note.py
@@ -1,7 +1,41 @@
-from itertools import *
+def solution(bandage, health, attacks):
+    b_time, sec_heal, add_heal = bandage
+    b_count = 0
+    
+    now_health = health
+    max_health = health
+    
+    attact_time = attacks[-1][0]
+    
+    
+    attack_dict = {}
+    
+    for attack in attacks:
+        attack_dict[attack[0]] = attack
+    
+    for i in range(1, attact_time + 1):
+        try:
+            now_health = now_health - attack_dict[i][1]
+            b_count = 0
+            if now_health <= 0:
+                now_health = -1
+                
+                break
+        except:
+            b_count = b_count + 1
+             
+            if now_health + sec_heal >= max_health:
+                now_health = max_health
+            else:
+                if b_count == b_time:
+                    now_health = now_health + sec_heal + add_heal
+                    
+                    if now_health + sec_heal + add_heal >= max_health:
+                        now_health = max_health
+                    b_count = 0
+                else:
+                    now_health = now_health + sec_heal
 
-def solution(list):
-    answer = [(a+b) for a,b in combinations(list, 2)]   
-    return sorted([*set(answer)])
+    return now_health
 
-print(solution([2,1,3,4,1,7]))
+solution([5, 1, 5],30,[[2, 10], [9, 15], [10, 5], [11, 5]])
